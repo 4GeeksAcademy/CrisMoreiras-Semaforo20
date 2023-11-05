4f705882c4369ad73a3d6c7dce28023232d6cf3e
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-11-05 23:07:08 +0000

    01

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-//import React, { useState } from 'react';
-
-
+//import React from "react";
+import React, { useState } from 'react';
 
 const Home=()=>{
+
+	const [colorRed,setGlowRed] = useState ("grey");
+	const [colorYellow,setGlowYellow] = useState ("grey");
+
+	function glowRed () {
+		if (colorRed == "red"){
+			setGlowRed("grey")}
+		else{
+			setGlowRed("red")}
+	}
+
+	function glowYellow () {
+		if (colorYellow == "Yellow"){
+			setGlowYellow("grey")}
+		else{
+			setGlowYellow("Yellow")}
+	}
+	
+
 	return (
 		<>
-		<button>soy rojo</button>
-		<button>soy ambar</button>
-		<button>soy verde</button>
+		<button onClick={glowRed} style={{backgroundColor:colorRed}}>ROJO</button>
+		<button onClick={glowYellow} style={{backgroundColor:colorYellow}}>AMBAR</button>
+		<button style={{backgroundColor: "green"}}>VERDE</button>
 		</>	
 		
 	);
