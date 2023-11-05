ee53fcad25d449256cfa36fcdb45801c861fd37f
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-11-05 23:26:57 +0000

    02

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,19 +5,37 @@ const Home=()=>{
 
 	const [colorRed,setGlowRed] = useState ("grey");
 	const [colorYellow,setGlowYellow] = useState ("grey");
+	const [colorGreen,setGlowGreen] = useState ("grey");
 
 	function glowRed () {
 		if (colorRed == "red"){
 			setGlowRed("grey")}
 		else{
-			setGlowRed("red")}
+			setGlowRed("red");
+			setGlowYellow("grey");
+			setGlowGreen("grey");
+		}
 	}
 
 	function glowYellow () {
-		if (colorYellow == "Yellow"){
+		if (colorYellow == "yellow"){
 			setGlowYellow("grey")}
 		else{
-			setGlowYellow("Yellow")}
+			setGlowYellow("yellow");
+			setGlowRed("grey");
+			setGlowGreen("grey");
+		}		
+	}
+
+	function glowGreen () {
+		if (colorGreen == "green"){
+			setGlowGreen("grey")}
+		else{
+			setGlowGreen("green");
+			setGlowRed("grey");
+			setGlowYellow("grey");
+		}
+			
 	}
 	
 
@@ -25,9 +43,8 @@ const Home=()=>{
 		<>
 		<button onClick={glowRed} style={{backgroundColor:colorRed}}>ROJO</button>
 		<button onClick={glowYellow} style={{backgroundColor:colorYellow}}>AMBAR</button>
-		<button style={{backgroundColor: "green"}}>VERDE</button>
-		</>	
-		
+		<button onClick={glowGreen} style={{backgroundColor:colorGreen}}>VERDE</button>
+		</>		
 	);
 };
 
